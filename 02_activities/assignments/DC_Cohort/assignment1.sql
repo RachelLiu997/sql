b4c4f80c0f1e541dfc4abad9fc305f12c5bbc72d
Rachel <rachel.luluu@gmail.com>
2025-11-06 12:29:10 -0500

    Finished one part portion of the assignment

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -4,17 +4,23 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT *
+FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT*
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
-
+SELECT *
+FROM customer_purchases 
+WHERE product_id IN (4,9);
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -23,11 +29,25 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT 
+customer_id,
+product_id,
+quantity,
+cost_to_customer_per_qty,
+quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id >= 8 AND customer_id <= 10;
 
 -- option 2
 
-
+SELECT 
+customer_id,
+product_id,
+quantity,
+cost_to_customer_per_qty,
+quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10;
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -35,19 +55,41 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+SELECT
+product_name,
+product_id,
+CASE 
+	WHEN product_qty_type LIKE '%unit%' THEN 'unit'
+	ELSE 'bulk'
+	END as product_qty_type_condensed
 
+FROM  product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT
+product_name,
+product_id,
+CASE 
+	WHEN product_name LIKE '%pepper%' THEN 1
+	ELSE 0
+	END as pepper_flag
+
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
-
+SELECT *
+FROM vendor as v
+INNER JOIN vendor_booth_assignments as vb
+	ON v.vendor_id = vb.vendor_id
+ORDER BY vendor_name, market_date;
+	
 
 
 /* SECTION 3 */
@@ -56,7 +98,11 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+SELECT 
+vendor_id,
+COUNT (vendor_id) as num_of_booth_rented
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -64,7 +110,18 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
+SELECT 
+customer_last_name, 
+customer_first_name,
+ROUND(SUM(quantity*cost_to_customer_per_qty),2) as total_spend
 
+FROM customer_purchases as cp
+INNER JOIN customer as p
+	ON cp.customer_id = p.customer_id
+
+GROUP BY cp.customer_id 
+HAVING total_spend > 2000
+ORDER BY customer_last_name, customer_first_name;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -78,8 +135,17 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+DROP TABLE IF EXISTS temp.new_vendor;
 
+INSERT OR IGNORE INTO vendor(vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'a Fresh Focused store', 'Thomas', 'Rosental');
 
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
+
+	
+	
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
 
